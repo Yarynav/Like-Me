7291ada5f4bb03fcab4dc2ec14d982a48010ffd5
yary navarrete <yar.navarrete@gmail.com>
2023-01-01 19:30:41 -0300

    ADD: basic configuration of the backend into the LIKE ME project

---
+++ DataBase.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const { config } = require('dotenv');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  allowExitOnIdle: true,
+});
+
+module.exports = pool;
---
+++ controllers/PostController.js
@@ -0,0 +1,35 @@
+const pool = require('../DataBase');
+
+const createPost = async (req, res) => {
+  const payload = req.body;
+  //
+  const query =
+    'INSERT INTO posts (titulo, img, descripcion, likes) VALUES ($1, $2, $3, $4) RETURNING *'; //consulta sql
+  const values = [
+    payload.titulo,
+    payload.url,
+    payload.descripcion,
+    payload.likes,
+  ]; //los valores que se van a insertar en la base de datos de esta forma se evita el sql injection
+  const result = await pool.query(query, values);
+  res.json(result.rows);
+};
+
+const getPosts = async (req, res) => {
+  const { rows } = await pool.query('SELECT * FROM posts');
+  res.json(rows);
+};
+const deletePost = async (req, res) => {
+  const id = req.params.id;
+  const query = 'DELETE FROM posts WHERE id = $1';
+  const value = [id];
+
+  const resultDelet = await pool.query(query, value);
+  res.json(resultDelet);
+};
+
+module.exports = {
+  createPost,
+  getPosts,
+  deletePost,
+};
